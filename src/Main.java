452aa0502b35a8242b6fc034316dd8281efc36f3
Krzysztof Kampa <krzysztofk7777@gmail.com>
2020-02-26 23:04:05 +0100

    TaskB added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-import TaskA.TaskA;
-
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("------------------ Task A ------------------"+"/n");
         TaskA.taskA();
+        System.out.println("------------------ Task B ------------------"+"/n");
+        TaskB.taskB();
+
+
     }
 }
